b9c295a220286a8795374a4c2056ef1710f6c55f
Nicholas Hippenmeyer <nicholashippenmeyer@gmail.com>
2014-03-18 17:58:51 -0400

    Remove assets

--- lib/generators/spree_versioning/install/install_generator.rb
+++ lib/generators/spree_versioning/install/install_generator.rb
@@ -4,16 +4,6 @@ module SpreeVersioning
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
-      def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_versioning\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_versioning\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_versioning\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_versioning\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_versioning'
       end
