910ce53766b57bb911457dc78ae40e61c326e6f4
Jordan Brough <git.j@brgh.net>
2014-04-02 10:58:47 -0600

    fix for api controllers
    For Spree 2.1 api controllers we need to include PaperTrail manually.
    Also, there is a PaperTrail bug and some PaperTrail weirdness that we need to work around.  See code comments.

--- lib/spree_versioning/auth_helper.rb
+++ lib/spree_versioning/auth_helper.rb
@@ -1,5 +1,6 @@
 class ActionController::Base
+  # NOTE: This is overridden in Spree::Api::BaseController to use 'current_api_user'
   def user_for_paper_trail
-    respond_to?(:current_api_user) ? current_api_user : try_spree_current_user
+    try_spree_current_user
   end
 end
